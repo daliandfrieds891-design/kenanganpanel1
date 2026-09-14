9d1a4874adf4895fbade48b4c044d02ba1576dab
daliandfrieds891-design <daliandfrieds891@gmail.com>
2026-09-14 11:10:13 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://otjwjersymokmbvobkic.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_A1VDY7Y9F2zA1epD6z2ugA_L8Ni1Pnv";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
